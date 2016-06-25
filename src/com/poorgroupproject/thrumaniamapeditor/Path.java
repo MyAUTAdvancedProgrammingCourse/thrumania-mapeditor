88bf7ee3a1904daf17afeb572ecfc7ac99d3889d
anvari1313 <ahmad.anv@gmail.com>
2016-06-25 14:26:45 +0430

    changing the path

--- src/com/poorgroupproject/thrumaniamapeditor/Path.java
+++ src/com/poorgroupproject/thrumaniamapeditor/Path.java
@@ -4,7 +4,7 @@ package com.poorgroupproject.thrumaniamapeditor;
  * Created by ahmad on 5/28/16.
  */
 public class Path {
-    public static String panelImagePath = "resource/image/panel/";
+    public static String panelImagePath = "resource/image/me_panel/";
     public static String tileImagePath  = "resource/image/tile/";
 
 }
